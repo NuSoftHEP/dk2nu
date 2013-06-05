33649cf199e086184426acb139ba184f5b8e1ed3
Robert Hatcher <rhatcher@fnal.gov>
2013-06-05 19:52:03 +0000

    Alex Radovic's removal of a small angle approximation.

--- dk2nu/tree/calcLocationWeights.cxx
+++ dk2nu/tree/calcLocationWeights.cxx
@@ -195,8 +195,13 @@ int bsim::calcEnuWgt(const bsim::Decay& decay, const TVector3& xyz,
   enu = emrat * enuzr;  // the energy ... normally
 
   // Get solid angle/4pi for detector element
-  double sangdet = ( kRDET*kRDET / 
-                     ( (zpos-decay.vz)*(zpos-decay.vz) ) ) / 4.0;
+  // small angle approximation, fixed by Alex Radovic
+  //SAA//  double sangdet = ( kRDET*kRDET / 
+  //SAA//                   ( (zpos-decay.vz)*(zpos-decay.vz) ) ) / 4.0;
+  double sanddetcomp = TMath::Sqrt( ( (xpos-decay.vx)*(xpos-decay.vx) ) +
+                                    ( (ypos-decay.vy)*(ypos-decay.vy) ) +
+                                    ( (zpos-decay.vz)*(zpos-decay.vz) )   );
+  double sangdet = (1.0-TMath::Cos(TMath::ATan( kRDET / sanddetcomp )))/2.0;
 
   // Weight for solid angle and lorentz boost
   wgt_xy = sangdet * ( emrat * emrat );  // ! the weight ... normally
